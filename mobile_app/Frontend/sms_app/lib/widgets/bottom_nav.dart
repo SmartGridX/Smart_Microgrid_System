950fd98d82fa0ceb673845cd866bf5205e2567ff
Cipher-Shadow-IR <202154115+Cipher-Shadow-IR@users.noreply.github.com>
2025-09-22 22:47:21 +0530

    Feature: add bottom navigator bar and create profile section

---
+++ mobile_app/Frontend/sms_app/lib/widgets/bottom_nav.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class BottomNav extends StatelessWidget {
+  final int currentIndex;
+  final Function(int) onTap;
+
+  const BottomNav({super.key, required this.currentIndex, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      currentIndex: currentIndex,
+      onTap: onTap,
+      type: BottomNavigationBarType.fixed,
+      items: const [
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+        BottomNavigationBarItem(icon: Icon(Icons.show_chart), label: 'Charts'),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.notifications),
+          label: 'Alerts',
+        ),
+        BottomNavigationBarItem(icon: Icon(Icons.grid_view), label: 'Grid'),
+      ],
+    );
+  }
+}
